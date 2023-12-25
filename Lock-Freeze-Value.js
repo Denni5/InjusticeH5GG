67a0639442185cf67a167050d7b93b340b39862f
Dennis Petrenko <denis50@live.de>
2023-12-25 15:26:19 +0100

    up

--- Lock-Freeze-Value.js
+++ Lock-Freeze-Value.js
@@ -5,14 +5,16 @@ h5gg.searchNumber("123", "I32", "0x0", "0xFFFFFFFF00000000");
 var count = h5gg.getResultsCount();
 var results = h5gg.getResults(count);
 
-var locker = setInterval(function() {
-    console.log("running...");
-    for(var i=0; i<count; i++) {
-        h5gg.setValue(results[i].address, "456", "I32");
-    }
-},
-500  //lock/freeze time interval (millseconds)
-);
+alert(results.length);
+
+// var locker = setInterval(function() {
+//     console.log("running...");
+//     // for(var i=0; i<count; i++) {
+//     //     h5gg.setValue(results[i].address, "456", "I32");
+//     // }
+// },
+// 500  //lock/freeze time interval (millseconds)
+// );
 
 //then we can cancel the lock/freeze:
 //clearInterval(locker);
